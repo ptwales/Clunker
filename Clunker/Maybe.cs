f800535da0be4fefe39a9ecb7715ad28f112b590
Philip Wales <ptwales@openmailbox.org>
2016-05-10 12:29:32 -0500

    Lost previous work, rebuilding base Maybe Class

---
+++ Clunker/Maybe.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Clunker
+{
+    public class Maybe
+    {
+        private object _boxed;
+
+        public Maybe(object boxed)
+        {
+            _boxed = boxed;
+        }
+
+        public Maybe some(object boxed)
+        {
+            if (boxed != null)
+            {
+                return new Maybe(boxed);
+            }
+            else
+            {
+                throw new ArgumentNullException("boxed", "Cannot create some of null object, use maybe instead.");
+            }
+        }
+
+        public Maybe none()
+        {
+            return new Maybe(null);
+        }
+
+        public Maybe maybe(object unknown)
+        {
+            return new Maybe(unknown);
+        }
+
+        public bool isSome()
+        {
+            return (_boxed != null);
+        }
+
+        public bool isNone()
+        {
+            return !isSome();
+        }
+
+    }
+}
