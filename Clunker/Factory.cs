ae4689235d22270d65fc05824d5dd2b36dc1f5dc
Philip Wales <ptwales@openmailbox.org>
2016-05-10 22:16:08 -0500

    Factory now has all explicit constructor methods.

--- Clunker/Factory.cs
+++ Clunker/Factory.cs
@@ -7,7 +7,36 @@ namespace Clunker
     public class Factory
     {
         public Factory() {}
-        public Maybe maybe = new None();
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Clunker.Maybe"/> class.
+        /// Maybe will be none if object is null and some otherwise
+        /// </summary>
+        /// <param name="boxed">Object to contain, may be null.</param>
+        public Maybe maybe(object boxed) {
+            if (boxed != null) {
+                return new Some(boxed);
+            } else {
+                return new None();
+            }
+        }
+
+        /// <summary>
+        /// Returns a Maybe that contain's a non-null object.
+        /// Will error if null.
+        /// </summary>
+        /// <param name="boxed">Non-null object to contain.</param>
+        public Maybe some(object boxed) {
+            return new Some (boxed);
+        }
+
+        public Maybe none() {
+            return new None();
+        }
+
+        public Applicable onArgs(object obj, string method) {
+            return new OnArgs(obj, method);
+        }
     }
 }
 
--- Clunker/Maybe.cs
+++ Clunker/Maybe.cs
@@ -6,54 +6,14 @@ namespace Clunker
    
     public interface Maybe : Monadic, Showable
     {
-        Maybe maybe(object boxed);
-        Maybe some(object boxed);
-        Maybe none();
         bool isSome();
         bool isNone();
         object getItem();
         object getOrElse(object other);
     }
 
-    abstract class AbstractMaybe : Maybe
-    {
-        /// <summary>
-        /// Initializes a new instance of the <see cref="Clunker.Maybe"/> class.
-        /// Maybe will be none if object is null and some otherwise
-        /// </summary>
-        /// <param name="boxed">Object to contain, may be null.</param>
-        public Maybe maybe(object boxed) {
-            if (boxed != null) {
-                return new Some(boxed);
-            } else {
-                return new None();
-            }
-        }
-
-        /// <summary>
-        /// Returns a Maybe that contain's a non-null object.
-        /// Will error if null.
-        /// </summary>
-        /// <param name="boxed">Non-null object to contain.</param>
-        public Maybe some(object boxed) {
-            return new Some (boxed);
-        }
-
-        public Maybe none() {
-            return new None();
-        }
-
-        public abstract bool isSome();
-        public abstract bool isNone();
-        public abstract object getItem();
-        public abstract object getOrElse(object other);
-        public abstract Monadic map(Applicable f);
-        public abstract Monadic flatMap(Applicable f);
-        public abstract string show();
-    }
-
     [ClassInterface(ClassInterfaceType.AutoDual)]
-    class Some : AbstractMaybe
+    class Some : Maybe
     {
         /// <summary>
         /// Single value contained by the Maybe object.
@@ -68,68 +28,68 @@ namespace Clunker
             }
         }
 
-        public override bool isSome() {
+        public bool isSome() {
             return true;
         }
 
-        public override bool isNone() {
+        public bool isNone() {
             return false;
         }
 
-        public override object getItem() {
+        public object getItem() {
             return _boxed;
         }
 
-        public override object getOrElse(object other) {
+        public object getOrElse(object other) {
             return _boxed;
         }
 
-        public override Monadic map(Applicable f) {
+        public Monadic map(Applicable f) {
             var result = f.apply(_boxed);
             return new Some(result);
         }
 
-        public override Monadic flatMap(Applicable f) {
+        public Monadic flatMap(Applicable f) {
             return (Maybe) f.apply (_boxed);
         }
 
-        public override string show() {
+        public string show() {
             return "Some(" + _boxed + ")";
         }
     }
 
     [ClassInterface(ClassInterfaceType.AutoDual)]
-    class None : AbstractMaybe 
+    class None : Maybe 
     {
         public None() {
           // nothing to do.    
         }
 
-        public override bool isSome() {
+        public bool isSome() {
             return false;
         }
 
-        public override bool isNone() {
+        public bool isNone() {
             return true;
         }
 
-        public override object getItem() {
+        public object getItem() {
             throw new NullReferenceException ("Calling getItem on None object");
         }
 
-        public override object getOrElse(object other) { 
+        public object getOrElse(object other) { 
             return other; 
         }
 
-        public override Monadic map (Applicable f) {
+        public Monadic map (Applicable f) {
             return new None ();
         }
 
-        public override Monadic flatMap (Applicable f) {
+        public Monadic flatMap (Applicable f) {
             return new None ();
         }
 
-        public override string show() {
+        public string show() {
             return "None()";
         }
     }
