9fcbcf9e9f5a4c8b8758e9ae058cc8377bb85d0a
Philip Wales <ptwales@openmailbox.org>
2016-05-10 22:13:40 -0500

    Mock up of showable framework.

---
+++ Clunker/OnArgs.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Reflection;
+
+namespace Clunker
+{
+    class OnObject : AbstractApplicable
+    {
+        private object _obj;
+        private MethodInfo _method;
+
+        public OnObject(object obj, string method) {
+            Type objType = obj.GetType();
+            _method = objType.GetMethod(method);
+            _obj = obj;
+        }
+
+        public override object applyOnArray(object[] args) {
+            return _method.Invoke(_obj, args);
+        }
+    }
+}
+
--- Clunker/Showable.cs
+++ Clunker/Showable.cs
@@ -1,5 +1,6 @@
 ﻿using System;
-
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Clunker
 {
@@ -7,4 +8,36 @@ namespace Clunker
     {
         string show();
     }
+
+    // I don't think we need this, it should just resort to ToString...
+    // Some of the most complicated code in VBEX was defShow.bas,
+    // but if C# will do all that jazz for free then why waste my time,
+    // or create difficult code to read.
+    abstract class AbstractShowable: Showable
+    {
+        
+        protected string showBySequence(IEnumerable<object> members) {
+            var shownMembers = showSequence(members);
+            var shownComposition = string.Join(", ", shownMembers);
+            var typeName = this.GetType().ToString();
+            return string.Format("{0}({1})", typeName, shownComposition);
+        }
+
+        protected string showParameters(params object[] members) {
+            return showBySequence(members);
+        }
+
+        protected IEnumerable<string> showSequence(IEnumerable<object> sequence) {
+            return from element in sequence select showElement(element);
+        }
+
+        protected string showElement(object element) {
+            return element.ToString();
+        }
+            
+        public string show() {
+            return this.ToString();
+        }
+    }
+
 }
