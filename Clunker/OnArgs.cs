18f55ce3d379c55ef451bec0d66ae254e5933b27
Philip Wales <ptwales@openmailbox.org>
2016-05-10 22:14:16 -0500

    created OnArgs and OnObject, must rename file.

--- Clunker/OnArgs.cs
+++ Clunker/OnArgs.cs
@@ -3,12 +3,12 @@ using System.Reflection;
 
 namespace Clunker
 {
-    class OnObject : AbstractApplicable
+    class OnArgs : AbstractApplicable
     {
         private object _obj;
         private MethodInfo _method;
 
-        public OnObject(object obj, string method) {
+        public OnArgs(object obj, string method) {
             Type objType = obj.GetType();
             _method = objType.GetMethod(method);
             _obj = obj;
@@ -17,6 +17,26 @@ namespace Clunker
         public override object applyOnArray(object[] args) {
             return _method.Invoke(_obj, args);
         }
+    }
+
+    class OnObject : AbstractApplicable 
+    {
+        private object[] _args;
+        private string _method;
+
+        public OnObject(string method, object[] args) {
+            _args = args;
+            _method = method;
+        }
+
+        public override object applyOnArray(object[] args) {
+            // TODO: assert args is size 1;
+            var obj = args[0];
+            Type objType = obj.GetType();
+            MethodInfo method = objType.GetMethod(_method);
+            return method.Invoke(obj, _args);
+        }
+
     }
 }
 
