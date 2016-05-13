19c63ad170a62b12ba0a1f55451762476f132607
Philip Wales <ptwales@openmailbox.org>
2016-05-13 16:25:40 -0500

    Unit Tests are working!

---
+++ ClunkerTests/AssocTest.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using System;
+using Clunker;
+
+namespace ClunkerTests
+{
+    [TestFixture()]
+    public class AssocTest
+    {
+        [Test()]
+        public void ConstructionTest()
+        {
+            Assoc a = new Assoc("key", "value");
+            Assert.AreEqual("key", a.key());
+            Assert.AreEqual("value", a.value());
+        }
+
+        [Test()]
+        public void OperationalTest()
+        {
+            Factory f = new Factory();
+            Assoc a = new Assoc('k', 'v');
+            Applicable op = f.onObject("key");
+            Assoc b = new Assoc(op, a);
+            Assert.AreEqual('k', b.value());
+        }
+    }
+}
+
