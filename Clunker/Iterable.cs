eb80bb9a7b5e8c943f65275e73348cf5c36b027d
Philip Wales <ptwales@openmailbox.org>
2016-06-29 10:51:36 -0500

    TraversableOnce not needed,yet.

--- Clunker/Iterable.cs
+++ Clunker/Iterable.cs
@@ -16,65 +16,9 @@ namespace Clunker.Collections
 		//Iterable dropWhile(Pred p);
 	}
 
-	public abstract class AbstractIterable : Iterable
+	public abstract class AbstractIterable : AbstractTraversable, Iterable
 	{
 		public abstract Iterator toIterator();
-
-        public abstract IEnumerator GetEnumerator();
-
-		public bool isEmpty()
-		{
-			return toIterator().isEmpty();
-		}
-
-		public object head()
-		{
-			return toIterator().head();
-		}
-
-		public object last()
-		{
-			return toIterator().last();
-		}
-
-		public Maybe maybeHead()
-		{
-			return toIterator().maybeHead();
-		}
-
-		public Maybe maybeLast()
-		{
-			return toIterator().maybeLast();
-		}
-
-		public Maybe find(Pred pred)
-		{
-			return toIterator().find(pred);
-		}
-
-		public Maybe findLast(Pred pred)
-		{
-			return toIterator().findLast(pred);
-		}
-
-		public int countWhere(Pred pred)
-		{
-			return toIterator().countWhere(pred);
-		}
-			
-		public object foldLeft(object z, Func2 f)
-		{
-			return toIterator().foldLeft(z, f);
-		}
-
-		//object foldRight(object z, Func2 f);
-
-		public object reduceLeft(Func2 f)
-		{
-			return toIterator().reduceLeft(f);
-		}
-
-		//object reduceRight(Func2 f);
 	}
 }
 
--- Clunker/Iterator.cs
+++ Clunker/Iterator.cs
@@ -4,7 +4,7 @@ using System.Linq;
 
 namespace Clunker.Collections
 {
-    public interface Iterator: TraversableOnce
+    public interface Iterator: Traversable
     {
 		/// <summary>
 		/// Check if there iterator has another value.
@@ -19,100 +19,17 @@ namespace Clunker.Collections
 		object next();
     }
 
-	public abstract class AbstractIterator: Iterator
+    public abstract class AbstractIterator: AbstractTraversable, Iterator
 	{
 		// Methods that must be implemented
 		public abstract bool hasNext();
 		public abstract object next();
-
-		public bool isEmpty()
-		{
-			return !hasNext();
-		}
-
-		public object head()
-		{
-			return next();
-		}
-
-		public object last()
-		{
-			object result = null;
-			while (hasNext()) {
-				result = next();
-			}
-			return result;
-		}
-
-		public Maybe maybeHead()
-		{
-			try {
-				return new Some(head());
-			} catch {
-				return new None();
-			}
-		}
-
-		public Maybe maybeLast()
-		{
-			try {
-				return new Some(last());
-			} catch {
-				return new None();
-			}
-		}
-
-		public Maybe find(Pred pred)
-		{
-			while (hasNext()) {
-				object x = next();
-				if (pred.apply(x)) {
-					return new Some(x);
-				}
-			}
-			return new None();
-		}
-
-		public Maybe findLast(Pred pred)
-		{
-			object lastFound = null;
-			while (hasNext()) {
-				object x = next();
-				if (pred.apply(x)) {
-					lastFound = x;
-				}
-			}
-			if (lastFound != null) {
-				return new Some(lastFound);
-			} else {
-				return new None();
-			}
-		}
-
-		public int countWhere(Pred pred)
-		{
-			int c = 0;
-			while (hasNext()) {
-				if (pred.apply(next())) {
-					++c;
-				}
-			}
-			return c;
-		}
-
-		public object foldLeft(object z, Func2 f)
-		{
-			var result = z;
-			while (hasNext()) {
-				result = f.apply(result, next());
-			}
-			return result;
-		}
-
-		public object reduceLeft(Func2 f)
-		{
-			return foldLeft(head(), f);
-		}
+        override public IEnumerator GetEnumerator()
+        {
+            while (hasNext()) {
+                yield return next();
+            }
+        }
 	}
 
 	class CompatIterator: AbstractIterator
