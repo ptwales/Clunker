bc77beaf796660f37a0a797a02482f93207e87c3
Philip Wales <ptwales@openmailbox.org>
2016-06-30 18:33:07 -0500

    Maybes don't inherit showable via COM?

--- ClunkerTests/MaybeTest.cs
+++ ClunkerTests/MaybeTest.cs
@@ -7,12 +7,12 @@ namespace ClunkerTests
     [TestFixture()]
     public class MaybeTest
     {
-        private Factory f = new Factory();
+        private Factory clunk = new Factory();
 
         [Test()]
         public void maybeSomeConstructor()
         {
-            Maybe some = f.maybe(0);
+            Maybe some = clunk.maybe(0);
             Assert.IsTrue(some.isSome());
             Assert.AreEqual(0, some.getItem());
         }
@@ -20,7 +20,7 @@ namespace ClunkerTests
         [Test()]
         public void maybeNoneConstructor()
         {
-            Maybe none = f.maybe(null);
+            Maybe none = clunk.maybe(null);
             Assert.IsTrue(none.isNone());
             Assert.AreEqual(12, none.getOrElse(12));
         }
@@ -28,16 +28,25 @@ namespace ClunkerTests
         [Test()]
         public void someConstructor()
         {
-            Maybe some = f.some(1);
+            Maybe some = clunk.some(1);
             Assert.AreEqual(1, some.getItem());
             try {
-                f.some(null);
+                clunk.some(null);
                 Assert.Fail("Didn't raise an exception at all.");
             } catch (ArgumentNullException ane) {
                 Assert.Pass("Raised expected exception: " + ane.ToString());
             } catch (Exception e) {
                 Assert.Fail("Raised unknown exception: " + e.ToString());
             }
+        }
+        
+        [Test()]
+        public void showingMaybe()
+        {
+            Maybe some = clunk.some(1);
+            Maybe none = clunk.none();
+            Assert.AreEqual("Some(1)", some.show());
+            Assert.AreEqual("None()", none.show());
         }
 
         //[Test()]
